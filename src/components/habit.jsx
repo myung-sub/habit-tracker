97934b14ce73480d2cf633258ed4531c44d47b84
myungsub.song <myungsub.song@gmail.com>
2021-02-11 16:47:41 +0900

    4.8 강의분

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -15,10 +15,11 @@ class Habit extends Component {
   }
 
   render() {
+    const {name, count} = this.props.habit;
     return (
       <li className="habit">
-      <span className="habit-name">reading</span>
-      <span className="habit-count">{this.state.count}</span>
+      <span className="habit-name">{name}</span>
+      <span className="habit-count">{count}</span>
       <button 
         className="habit-button habit-increase" 
         onClick={this.handleIncreament}>
